4ff0d0d4c76f2b130a1c8dc5f4d9e4a5577a28ec
yasinenis <yasin.enis.tr>
2024-04-07 13:58:18 +0300

    📕 committed before archive the folder

---
+++ example-12-3/main.c
@@ -0,0 +1,31 @@
+// ex12-3 : Using Nested if...else
+#include <stdio.h>
+
+int main() {
+
+	double n1, n2, n3;
+
+	printf("Enter three numbers: ");
+	scanf_s("%lf %lf %lf", &n1, &n2, &n3);
+
+	// outer if statement
+	if (n1 >= n2) {
+
+		// inner if...else
+		if (n1 >= n3)
+			printf("%.2lf is the largest number.", n1);
+		else
+			printf("%.2lf is the largest number.", n3);
+	}
+	
+	// outer else statement
+	else {
+
+		// inner if...else
+		if (n2 >= n3)
+			printf("%.2lf is the largest number.", n2);
+		else
+			printf("%.2lf is the largest number.", n3);
+	}
+	return 0;
+}
